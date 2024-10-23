a7e0ab5b8b3d9d0639f13fe7014b6dc4e616dabf
AndyUgansk <andyugansk@gmail.com>
2024-10-24 00:25:38 +0500

    Add files via upload

--- module_10_2.py
+++ module_10_2.py
@@ -9,14 +9,18 @@ class Knight(Thread):
         self.power = power
         self.enemies = 100
         self.days = 0
+
     def run(self):
         print(f'{self.name}, на нас напали!')
         while self.enemies > 0:
-            sleep(1)
-            self.days +=1
+            self.days += 1
             self.enemies -= self.power
+            if self.enemies < self.power:
+                self.enemies = 0
             print(f'{self.name} сражается {self.days} день(дня)..., осталось {self.enemies} воинов.')
+            sleep(1)
         print(f'{self.name} одержал победу спустя {self.days} дней(дня)!')
+
 
 first_knight = Knight('Sir Lancelot', 10)
 second_knight = Knight("Sir Galahad", 20)
